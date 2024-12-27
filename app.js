efcdf0363295d983d90819b6226655283fe1e614
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2024-12-28 02:33:36 +0800

    add error page

--- app.js
+++ app.js
@@ -3,20 +3,29 @@ const express = require("express");
 const app = express();
 const indexRouter = require("./routes/indexRouter");
 const genreRouter = require("./routes/genreRouter");
-const recordRouter = require("./routes/recordRouter")
+const recordRouter = require("./routes/recordRouter");
 const path = require("node:path");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 app.use("/genre", genreRouter);
 app.use("/record", recordRouter);
 
+app.use((req, res) => {
+    res.status(404).render("errorPage", {title: "Not Found"});
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
+});
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Listening to port ${PORT}`);
-})
+  console.log(`Listening to port ${PORT}`);
+});
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,10 @@
 const db = require("../db/queries");
+const asyncHandler = require("express-async-handler");
 
-const getIndexPage = async (req, res) => {
+const getIndexPage = asyncHandler(async (req, res) => {
   const genres = await db.getAllGenres();
   res.render("index", { title: "Home Page", genres });
-};
+});
 
 module.exports = {
   getIndexPage,
