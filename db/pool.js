13d09b9c977203881d5c4bb57699eb26a62e7484
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-01-01 05:18:01 +0800

    update connection string

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  connectionString: `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`
+  connectionString: `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?sslmode=require`
 });
