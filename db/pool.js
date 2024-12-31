2e10bb5da6b99c7a0ac4db5ce7a21f51060a842f
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-01-01 05:31:13 +0800

    fix connection issue

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,9 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  connectionString: `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?sslmode=require`
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  ssl: 'require',
 });
